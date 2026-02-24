3642daa1d61e5f318849bf53d5b5bc03ef2ea42d
HexDevourSoap <forgottenbabo@gmail.com>
2026-02-24 12:05:24 +0200

    first commit

---
+++ src/main/java/lv/venta/fidi/FidiApplication.java
@@ -0,0 +1,13 @@
+package lv.venta.fidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/venta/fidi/FidiApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta.fidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
